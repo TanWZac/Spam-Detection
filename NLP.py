bd8b104f6517101a2aa8d667813bf9d7824e8c9c
Wen Zheng Tan <100010968+TanWZac@users.noreply.github.com>
2022-11-17 22:53:11 +1100

    Added KNN and Naive Bayes

--- NLP.py
+++ NLP.py
@@ -12,7 +12,7 @@ df
 # In[2]:
 
 
-df.head(50)
+df.head()
 
 
 # In[3]:
@@ -52,7 +52,7 @@ from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 
 
-# In[7]:
+# In[8]:
 
 
 import string
@@ -60,7 +60,7 @@ import re
 clear = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
 
 
-# In[8]:
+# In[9]:
 
 
 lem = WordNetLemmatizer()
@@ -76,13 +76,13 @@ def process(msg):
     
 
 
-# In[9]:
+# In[10]:
 
 
 process(df['MESSAGE'])
 
 
-# In[10]:
+# In[11]:
 
 
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -91,20 +91,20 @@ X = tfidf.fit_transform(corpus)
 y = df["CATEGORY"]
 
 
-# In[11]:
+# In[72]:
 
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 
-# In[16]:
+# In[61]:
 
 
 import seaborn as sns
 
 
-# In[17]:
+# In[62]:
 
 
 import xgboost as xg
@@ -112,20 +112,7 @@ classifierXg = xg.XGBClassifier()
 classifierXg.fit(X_train, y_train)
 
 
-# In[18]:
-
-
-y_pred = classifierXg.predict(X_test)
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-acc = accuracy_score(y_pred, y_test)
-report = classification_report(y_test, y_pred)
-print(report)
-cm = confusion_matrix(y_pred, y_test)
-sns.heatmap(cm, annot=True)
-print("Accuracy of LightGBM Model:", acc*100,"%")
-
-
-# In[19]:
+# In[63]:
 
 
 import lightgbm as lgb
@@ -133,15 +120,56 @@ classifier = lgb.LGBMClassifier()
 classifier.fit(X_train, y_train)
 
 
+# In[64]:
+
+
+from sklearn.neighbors import KNeighborsClassifier
+classifierKNN = KNeighborsClassifier(n_neighbors=2)
+classifierKNN.fit(X_train, y_train)
+
+
+# In[87]:
+
+
+from sklearn.naive_bayes import MultinomialNB
+NB = MultinomialNB()
+NB.fit(X_train, y_train)
+
+
 # In[20]:
 
 
-y_pred = classifier.predict(X_test)
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-acc = accuracy_score(y_pred, y_test)
-report = classification_report(y_test, y_pred)
-print(report)
-cm = confusion_matrix(y_pred, y_test)
-sns.heatmap(cm, annot=True)
-print("Accuracy of LightGBM Model:", acc*100,"%")
+def report(model_name, X_test, y_test, model):
+    y_pred = model.predict(X_test)
+    from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+    acc = accuracy_score(y_pred, y_test)
+    report = classification_report(y_test, y_pred)
+    print(report)
+    cm = confusion_matrix(y_pred, y_test)
+    sns.heatmap(cm, annot=True)
+    print("Accuracy of " + model_name + " Model:", acc*100,"%")
+
+
+# In[22]:
+
+
+report("XGBoost", X_test, y_test, classifierXg)
+
+
+# In[23]:
+
+
+report("lightgbm", X_test, y_test, classifier)
+
+
+# In[29]:
+
+
+report("KNN", X_test, y_test, classifierKNN)
+
+
+# In[88]:
+
+
+report("Naive Bayes", X_test, y_test, NB)
 
